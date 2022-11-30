1063c306fc0c2e4763a8c1bd8e4e53151ad2deb4
Sukanta Purkayastha <30552975+sukanta7660@users.noreply.github.com>
2022-11-30 15:47:17 +0600

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import './assets/main.css';
 
 const app = createApp(App);
 
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 app.use(router);
 
-app.mount('#app');
+app.use(vuetify).mount('#app');
